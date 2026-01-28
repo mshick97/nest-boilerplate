44b7bcdfb3875ab5c91ad245107762ba90c78763
Maxwell <maxwellshick@gmail.com>
2026-01-28 14:40:26 -0500

    vitest init

---
+++ vitest.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vitest/config'
+
+export default defineConfig({
+    test: {
+        projects: [
+            {
+                test: {include: [
+                    './src/**/*.{test,spec}.ts'
+                ]}
+            }
+        ]
+    }
+})
